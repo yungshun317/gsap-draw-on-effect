93a87bbe8e1dd93fbd5c9fe832837b14edddb035
yungshun317 <seraph317@gmail.com>
2023-05-02 17:25:37 +0800

    [FEAT] Control the animation with the `onclick` events and use the `MotionPath` plugin

--- script.js
+++ script.js
@@ -8,10 +8,18 @@ let timeline = gsap.timeline({
         ease: "power1.inOut"
     }
 }).to("#hand", {
-    path: "#path",
-    align: "#path",
-    alignOrigin: [0.28, 0.08]
+    motionPath: {
+        path: "#path",
+        align: "#path",
+        alignOrigin: [0.28, 0.08]
+    }
 }).to("#path", {
         strokeDasharray: "4096, " + "0"
     }, "<"
 );
+
+document.getElementById("pause").onclick = () => timeline.pause();
+document.getElementById("play").onclick = () => timeline.play();
+document.getElementById("reverse").onclick = () => timeline.reverse();
+document.getElementById("seek").onclick = () => timeline.seek(5);
+document.getElementById("restart").onclick = () => timeline.restart();
